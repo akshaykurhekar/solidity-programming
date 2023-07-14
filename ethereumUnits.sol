0ce90feebed3482fae2e2d9664ba1bd7024a4c3b
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2023-07-14 15:52:08 +0530

    ep-6 update

--- ethereumUnits.sol
+++ ethereumUnits.sol
@@ -9,7 +9,17 @@ contract EtherUnits {
     uint public oneEther = 1 ether;
     // 1 ether is equal to 10^18 wei
     bool public isOneEther = 1 ether == 1e18;
+
+    // 1 ether is equal to 10^9 gwei ( 1 gwei = 10^9 wei);
+    uint public oneGWei = 1 gwei;
+
 }
 
-// 1000000000000000000
-//  123456789123456789 == 10e18 == 10^18
+// 1000000000000000000 = 10^18 wei = 1 ether =1e18
+//  123456789123456789 = 18
+
+//  1000000000 = 10^9 wei = 1 gwei
+//   123456789
+
+// ether > gwei > wei
+// 10^18 > 10^9 > 1
