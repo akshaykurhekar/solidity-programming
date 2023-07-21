48ef69072d8085ee9e49cacf8fa7395a3483d0ae
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2023-07-21 21:53:51 +0530

    ep-8 content added

---
+++ gas.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Gas {
+    uint public i = 0;
+    address public owner;
+
+
+    function gasTest() public returns(address){
+        owner = msg.sender;
+        return owner;
+    }
+
+    // Using up all of the gas that you send causes your transaction to fail.
+    // State changes are undone.
+    // Gas spent are not refunded.
+    function forever() public {
+        // Here we run a loop until all of the gas are spent
+        // and the transaction fails
+        while (true) {
+            i += 1;
+        }
+    }
+}
